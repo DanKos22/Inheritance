4f0b84f7204c49cc09100818f578900bddd235aa
Dan Koskiranta <G00397054@atu.ie>
2022-11-28 10:36:48 +0000

    Customer class tested

--- src/main/java/ie/atu/week10/TestPerson.java
+++ src/main/java/ie/atu/week10/TestPerson.java
@@ -7,6 +7,12 @@ public class TestPerson {
         Mike.setAddress("Newcastle");
         Mike.setNumber("1234");
 
-        System.out.println("\nHere are Mike's details: " + Mike.getName());
+        System.out.println("\nHere are Mike's details: " + Mike.toString());
+
+        // test customer
+        Customer person_1 = new Customer("Frank", "Newcastle", "0500", "222", true);
+
+        System.out.println("\nHere are the details for the customer: " + person_1.toString());
+
     }
 }
