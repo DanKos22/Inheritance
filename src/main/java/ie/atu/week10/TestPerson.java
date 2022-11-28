aba2d870cdce34e29ff724bbeb5177a65a1da363
Dan Koskiranta <G00397054@atu.ie>
2022-11-28 11:13:27 +0000

    Display the information using the toString and get methods

--- src/main/java/ie/atu/week10/TestPerson.java
+++ src/main/java/ie/atu/week10/TestPerson.java
@@ -9,10 +9,18 @@ public class TestPerson {
 
         System.out.println("\nHere are Mike's details: " + Mike.toString());
 
-        // test customer
-        Customer person_1 = new Customer("Frank", "Newcastle", "0500", "222", true);
+        // Use the toString to display the information
+        Customer person_1 = new Customer("Frank", "Newcastle", "0500", "666", true);
+        System.out.println("Here are the details of the customer: " + person_1.toString());
 
-        System.out.println("\nHere are the details for the customer: " + person_1.toString());
+        // Use the set methods to set the information and the get methods to display the information
+        person_1.setName("Frank");
+        person_1.setAddress("Newcastle");
+        person_1.setNumber("0500");
+        person_1.setCustomerNumber("222");
+        person_1.setMailingList(true);
+
+        System.out.println("\nHere are the details for the customer: name: " + person_1.getName() + ",\t address:\t"  + person_1.getAddress() + ",\t phone number:\t" + person_1.getNumber() + ",\t customer number:\t" + person_1.getCustomerNumber() + ",\t is on mailing list:\t" + person_1.isMailingList());
 
     }
 }
